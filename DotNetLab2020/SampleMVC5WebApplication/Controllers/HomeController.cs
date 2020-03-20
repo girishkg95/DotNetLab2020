5821c320b0cad8a2faeab81c9cf8fd23ce2d1fff
girishkg95 <girishkg95@gmail.com>
2020-03-20 18:43:37 +0530

    Nuget packages updated for the solution

--- DotNetLab2020/SampleMVC5WebApplication/Controllers/HomeController.cs
+++ DotNetLab2020/SampleMVC5WebApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SampleMVC5WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page!";
 
             return View();
         }

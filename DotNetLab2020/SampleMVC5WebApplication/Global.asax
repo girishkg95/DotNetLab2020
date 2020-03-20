7b59d47ffb6f29cb3cc55629b7d44e8e3798bdab
girishkg95 <girishkg95@gmail.com>
2020-03-20 17:58:25 +0530

    Initial Solution Setup Files

---
+++ DotNetLab2020/SampleMVC5WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleMVC5WebApplication.MvcApplication" Language="C#" %>
---
+++ DotNetLab2020/SampleMVC5WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleMVC5WebApplication.Startup))]
+namespace SampleMVC5WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
